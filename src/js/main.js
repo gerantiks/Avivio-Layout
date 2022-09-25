ed941b5915148cdfa16e7a1ee009fa67ff96a738
Vladyslav <vlad.miu58@gmail.com>
2022-09-25 23:56:26 +0300

    The layout has been completed, a video playback button has been made (using a video). Also fix minor bugs.

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+/* Скрипт для шапки навигации */
 const mobileNavButton = document.querySelector(".mobile-nav-button");
 const mobileNavIcon = document.querySelector(".mobile-nav-button_icon");
 const mobileNav = document.querySelector(".MobiHeader");
@@ -6,4 +7,22 @@ mobileNavButton.addEventListener('click', function () {
     mobileNavIcon.classList.toggle('active');
     mobileNav.classList.toggle('active');
     document.body.classList.toggle('no-scroll');
+});
+
+/*Скрипт для кнопки проигрывания видео */
+const videoFile = document.querySelector("#video-story");
+const videoBtn = document.querySelector("#video-story-btn");
+const videoBtnPlay = document.querySelector("#video-btn-play");
+
+videoBtn.addEventListener('click', function () {
+    
+    if (videoFile.paused) {
+        videoFile.play();
+        videoBtnPlay.src ="src/img/Img/button_pause.svg"; /*  Прописиваем функцию, если видеофайл проигрывает 
+        меняет ссылку на картинку с паузой  */
+    } else {
+        videoFile. pause();
+        videoBtnPlay.src ="src/img/Img/play-white.png"; /* Если видео остановилось то меняет на кнопку пуск*/
+    } 
+    
 });
